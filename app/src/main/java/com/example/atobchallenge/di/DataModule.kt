1bf183aab0c3a516fea43da30754009f6d6799bf
Paulo Renato da Silva Galindo <paulogalindo94@hotmail.com>
2023-10-01 15:11:03 +0100

    feat: create hilt Data and Repository modules

---
+++ app/src/main/java/com/example/atobchallenge/di/DataModule.kt
@@ -0,0 +1,18 @@
+package com.example.atobchallenge.di
+
+import com.example.atobchallenge.data.remotedatasource.ProductListRemoteDataSource
+import com.example.atobchallenge.data.remotedatasource.ProductListRemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataModule {
+
+    @Binds
+    fun bindProductListRemoteDataSource(
+        dataSource: ProductListRemoteDataSourceImpl
+    ): ProductListRemoteDataSource
+}
---
+++ app/src/main/java/com/example/atobchallenge/di/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package com.example.atobchallenge.di
+
+import com.example.atobchallenge.data.repository.ProductsListRepositoryImpl
+import com.example.atobchallenge.domain.repository.ProductsListRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+
+    @Binds
+    fun bindProductListRepository(
+        repository: ProductsListRepositoryImpl
+    ): ProductsListRepository
+}
